fc949b246d9cad07ee71624969766b36bf450d1b
nenowo <38612806+nenowoDev@users.noreply.github.com>
2025-05-21 09:10:39 +0800

    f

--- src/components/ListPersons.vue
+++ src/components/ListPersons.vue
@@ -1,12 +1,7 @@
 <template>
     <h3>Person List</h3>
     <ul id="personList"></ul>
-    <h3>View One Person</h3>
-    <input type="number" id="indexInput" placeholder="Enter index" />
-    <div id="onePersonView" class="detail-box">
-        <em>No person at that index</em>
-    </div>
-
+    
 </template>
 <script>
 </script>
--- src/components/ViewPerson.vue
+++ src/components/ViewPerson.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-
+    <h3>View One Person</h3>
+    <input type="number" id="indexInput" placeholder="Enter index" />
+    <div id="onePersonView" class="detail-box">
+        <em>No person at that index</em>
     </div>
+
 </template>
 <script>
 </script>
