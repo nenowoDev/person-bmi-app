2addbdc4a46d63f2755a821d6692a90ac61d74e9
nenowo <38612806+nenowoDev@users.noreply.github.com>
2025-05-21 10:29:20 +0800

    Create route.js

---
+++ src/router/route.js
@@ -0,0 +1,21 @@
+import { createRouter,createWebHistory } from "vue-router";
+import AddPerson from "@/components/AddPerson.vue";
+import EditPerson from "@/components/EditPerson.vue"
+import DeletePerson from "@/components/DeletePerson.vue"
+import ListPersons from "@/components/ListPersons.vue";
+import ViewPerson from "@/components/ViewPerson.vue";
+import BmiText from "@/components/BmiText.vue";
+
+const router=createRouter({
+    history: createWebHistory(),
+    routes:[
+        {path:'/addperson',component:AddPerson},
+        {path:'/editperson',component:EditPerson},
+        {path:'/deleteperson',component:DeletePerson},
+        {path:'/listperson',component:ListPersons},
+        {path:'/viewperson',component:ViewPerson},
+        {path:'/stats',component:BmiText},
+    ]
+});
+
+export default router
