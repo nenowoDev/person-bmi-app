cf8a1d7696f8a5195d6ac429d0fa4d0d2a3120c7
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-05-21 09:54:21 +0800

    b

--- src/components/ViewPerson.vue
+++ src/components/ViewPerson.vue
@@ -1,10 +1,33 @@
 <template>
     <h3>View One Person</h3>
-    <input type="number" id="indexInput" placeholder="Enter index" />
+    <input type="number" id="indexInput" v-model.number="indexInput" placeholder="Enter index" />
     <div id="onePersonView" class="detail-box">
         <em>No person at that index</em>
     </div>
 
 </template>
 <script>
+ export default{
+    name:"viewPerson",
+    props:{
+        personList:{
+            type:Array,
+            required:true
+        }
+    },
+    data(){
+        return{
+            indexInput:''
+        }
+    },
+    methods:{
+        displayByindex(){
+          this.personList.forEach((item)=>{
+            if(item == this.indexInput)
+          }
+        )
+
+        }
+    }
+ }
 </script>
